b7c8a15655d91d87b2cf172242e535f02c1db3ec
Aarav-Nigam <avatar.aarav28@gmail.com>
2023-03-28 15:41:32 +0530

    changing styles of sort type

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Box, Tabs, Tab } from '@mui/material';
+import {Select,MenuItem} from '@mui/material';
 import './Tabs.css'
-export default function TabsPage({tab,setTab,search,setSearch,sortType,setSortType}) {
+export default function TabsPage({ tab, setTab, search, setSearch, sortType, setSortType }) {
 
     const handleChange = (event, newValue) => {
         setTab(newValue);
@@ -29,13 +30,22 @@ export default function TabsPage({tab,setTab,search,setSearch,sortType,setSortTy
                     <div id='searchBox' className='mx-3 rounded-pill border border-secondary border-2 d-flex align-items-center'>
                         &nbsp;
                         <i className="fa fa-search" aria-hidden="true"></i>
-                        <input id='search' value={search} onChange={(e) => { setSearch(e.target.value)}} type="search" className="rounded-pill border-0" placeholder="Search" aria-label="Search" />
+                        <input id='search' value={search} onChange={(e) => { setSearch(e.target.value) }} type="search" className="rounded-pill border-0" placeholder="Search" aria-label="Search" />
                     </div>
                     <div>
-                        <select id='select_sort_type' onChange={(e)=>setSortType(e.target.value)} className='mx-3 rounded-pill border border-secondary border-2 form-select' aria-label="Default select example">
+                        {/* <select id='select_sort_type' onChange={(e) => setSortType(e.target.value)} className='mx-3 rounded-pill border border-secondary border-2 form-select' aria-label="Default select example">
                             <option value={1}>Newest</option>
                             <option value={2}>Oldest</option>
-                        </select>
+                        </select> */}
+                        <Select
+                            id="select_sort_type"
+                            className='rounded-pill'
+                            value={sortType}
+                            onChange={(e) => setSortType(e.target.value)}
+                        >
+                            <MenuItem value={1}>Newest</MenuItem>
+                            <MenuItem value={2}>Oldest</MenuItem>
+                        </Select>
                         {/* {console.log(sortType)} */}
                     </div>
                 </div>
